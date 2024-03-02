e8714a2784f8cb59968ae66edb0b04c07797ded0
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-02 20:14:47 -0300

    fix: DynamoDB connection

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  region: process.env.AWS_DEFAULT_REGION,
+  region: "us-east-2",
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
