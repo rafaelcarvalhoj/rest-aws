e628a15c09dce74bd28fe657d0a27a465b703aca
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-02 20:17:18 -0300

    feat: Setting up User and Post Router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
-
+import userRouter from "./routes/user.routes";
+import postRouter from "./routes/post.routes";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello VTS!");
 });
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
