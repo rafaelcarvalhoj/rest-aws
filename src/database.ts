27cd11bc05efa0912df35e4e24d059003f36a546
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-02 18:44:58 -0300

    feat: Connection with AWS DynamoDB

---
+++ src/database.ts
@@ -0,0 +1,9 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  region: process.env.AWS_DEFAULT_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+export const client = new AWS.DynamoDB.DocumentClient();
