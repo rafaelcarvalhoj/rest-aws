2030b745431adbfca7936042226a703e9b87a393
Rafael Carvalho <rrafaelcarvalhoj@gmail.com>
2024-03-03 16:48:09 -0300

    chore: Running Port

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@ app.get("/", (req, res) => {
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
